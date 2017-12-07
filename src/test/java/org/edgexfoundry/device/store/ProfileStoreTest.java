bae1dcab1aa05e00dadd7ac2e5bfb2a37798bdaf
Jim White <james_white2@dell.com>
2017-12-06 22:51:16 -0600

    fix failing unit tests in ProfileStore - needed new mock object for the service object factory
    Signed-off-by: Jim White <james_white2@dell.com>

--- src/test/java/org/edgexfoundry/device/store/ProfileStoreTest.java
+++ src/test/java/org/edgexfoundry/device/store/ProfileStoreTest.java
@@ -33,6 +33,7 @@ import java.util.List;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.edgexfoundry.controller.DeviceProfileClient;
 import org.edgexfoundry.controller.ValueDescriptorClient;
+import org.edgexfoundry.device.domain.ServiceObjectFactory;
 import org.edgexfoundry.device.store.impl.ProfileStoreImpl;
 import org.edgexfoundry.domain.common.ValueDescriptor;
 import org.edgexfoundry.domain.meta.Device;
@@ -60,6 +61,9 @@ public class ProfileStoreTest {
 
   @Mock
   private DeviceProfileClient deviceProfileClient;
+  
+  @Mock
+  private ServiceObjectFactory serviceObjectFactory;
 
   private List<ValueDescriptor> valueDescriptors;
   private ValueDescriptor descriptor;
